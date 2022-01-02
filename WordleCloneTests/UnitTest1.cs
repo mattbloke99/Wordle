f7fffa940c407a61f001483a51b7ee854caa70cf
Matthew Hurrell <mattbloke99@googlemail.com>
2022-01-02 21:45:25 +0000

    Fixed for different scenarios

--- WordleCloneTests/UnitTest1.cs
+++ WordleCloneTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace WordleCloneTests
@@ -34,6 +35,34 @@ namespace WordleCloneTests
             Assert.False(row.Correct);
         }
 
+        [Fact]
+        public void Test3()
+        {
+            Row row = new Row("PILOT");
+            Flag[] result = row.Guess("TAAAT");
+
+            Assert.Equal(Flag.Partial, result[0]);
+            Assert.Equal(Flag.Wrong, result[1]);
+            Assert.Equal(Flag.Wrong, result[2]);
+            Assert.Equal(Flag.Wrong, result[3]);
+            Assert.Equal(Flag.Right, result[4]);
+            Assert.False(row.Correct);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Row row = new Row("PILOT");
+            Flag[] result = row.Guess("PILOT");
+
+            Assert.Equal(Flag.Right, result[0]);
+            Assert.Equal(Flag.Right, result[1]);
+            Assert.Equal(Flag.Right, result[2]);
+            Assert.Equal(Flag.Right, result[3]);
+            Assert.Equal(Flag.Right, result[4]);
+            Assert.True(row.Correct);
+        }
+
     }
 
     public class Row
@@ -48,25 +77,22 @@ namespace WordleCloneTests
 
         internal Flag[] Guess(string guess)
         {
-            var ret = new Flag[Answer.Length];
+            var ret = Enumerable.Repeat(Flag.Wrong, Answer.Length).ToArray();
 
             int i = 0;
-            foreach (var letter in Answer)
+            foreach (var letter in guess)
             {
 
-                ret[0] = Flag.Wrong;
-
-                if (guess.Equals(letter))
+                if (Answer[i].Equals(letter))
                 {
-                    ret[0] = Flag.Right;
-                } else if (guess.Contains(letter))
+                    ret[i] = Flag.Right;
+                } else if (Answer.Contains(letter))
                 {
-                    ret[0] = Flag.Partial;
+                    ret[i] = Flag.Partial;
                 }
                 i++;
 
             }
-
 
             Correct = Answer.Equals(guess,StringComparison.InvariantCultureIgnoreCase);
             return ret;
