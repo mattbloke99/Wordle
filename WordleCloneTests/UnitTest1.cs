2a486b11b74e8309e2a14c055779850dd526f288
Matthew Hurrell <mattbloke99@googlemail.com>
2022-01-02 21:18:51 +0000

    Guess wrong position

--- WordleCloneTests/UnitTest1.cs
+++ WordleCloneTests/UnitTest1.cs
@@ -13,8 +13,27 @@ namespace WordleCloneTests
             Flag[] result =  row.Guess("AAAAA");
 
             Assert.Equal(Flag.Wrong, result[0]);
+            Assert.Equal(Flag.Wrong, result[1]);
+            Assert.Equal(Flag.Wrong, result[2]);
+            Assert.Equal(Flag.Wrong, result[3]);
+            Assert.Equal(Flag.Wrong, result[4]);
             Assert.False(row.Correct);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            Row row = new Row("PILOT");
+            Flag[] result = row.Guess("TAAAA");
+
+            Assert.Equal(Flag.Partial, result[0]);
+            Assert.Equal(Flag.Wrong, result[1]);
+            Assert.Equal(Flag.Wrong, result[2]);
+            Assert.Equal(Flag.Wrong, result[3]);
+            Assert.Equal(Flag.Wrong, result[4]);
+            Assert.False(row.Correct);
+        }
+
     }
 
     public class Row
@@ -29,8 +48,28 @@ namespace WordleCloneTests
 
         internal Flag[] Guess(string guess)
         {
-            Correct = false;
-            return new[] { Flag.Wrong, Flag.Wrong, Flag.Wrong, Flag.Wrong, Flag.Wrong };
+            var ret = new Flag[Answer.Length];
+
+            int i = 0;
+            foreach (var letter in Answer)
+            {
+
+                ret[0] = Flag.Wrong;
+
+                if (guess.Equals(letter))
+                {
+                    ret[0] = Flag.Right;
+                } else if (guess.Contains(letter))
+                {
+                    ret[0] = Flag.Partial;
+                }
+                i++;
+
+            }
+
+
+            Correct = Answer.Equals(guess,StringComparison.InvariantCultureIgnoreCase);
+            return ret;
         }
     }
 
